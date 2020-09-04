679ad9df7a60eae4630f5b63366f9fb6492cfc4a
Amaan Nawab <nawab.mohdamaan13@gmail.com>
2020-09-05 00:00:18 +0530

    adding files for github pagessssssss

--- src/App.js
+++ src/App.js
@@ -16,8 +16,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
+    fetch('')
       .then(users => this.setState({ monsters: userData }));
   }
 
